76201627ac0d98c132565ac36a577804c36bac8a
Aldir Osmundo Tiburtino Neto <aldirneto.dev@gmail.com>
2026-05-12 22:28:31 -0300

    Feat: add base inicial do projeto

---
+++ src/main/java/com/aldirneto/mygamerboxd/MygamerboxdApplication.java
@@ -0,0 +1,13 @@
+package com.aldirneto.mygamerboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygamerboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygamerboxdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldirneto/mygamerboxd/MygamerboxdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldirneto.mygamerboxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygamerboxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
